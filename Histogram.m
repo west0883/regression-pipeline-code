7782e4136be9d777518ba25ec20c621b8c77ff44
Sarah West <west0883@umn.edu>
2022-05-26 09:53:21 -0500

    Saves values and bin edges of histograms

--- Histogram.m
+++ Histogram.m
@@ -6,6 +6,30 @@
 
 function [parameters] = Histogram(parameters)
 
-    parameters.histogram = histogram(parameters.data, parameters.nbins);
+    % If there's a "values" field from RunAnalysis, print updating message
+    % for user. 
+    if isfield(parameters, 'values')
+        message = ['Histogram for '];
+        for dispi = 1:numel(parameters.values)/2
+           message = [message ', ' parameters.values{dispi}];
+        end
+        disp(message); 
+    end
     
+    % Find value ranges for bottom of histogram. 
+    dx = (parameters.histogram_value_range(2) - parameters.histogram_value_range(1)) ./ parameters.histogram_nBins;
+    x = (parameters.histogram_value_range(1) + dx): dx : parameters.histogram_value_range(2); 
+
+    % Don't allow figures to pop up.
+    set(groot,'defaultFigureVisible','off');
+
+    % Calculate histogram
+    h = histogram(parameters.data, [parameters.histogram_value_range(1) x]);
+
+    % Re-allow figures to pop up.
+    set(groot,'defaultFigureVisible','on');
+
+    % Put value ranges and values into the output. 
+    parameters.histogram = [x' h.Values'];
+
 end 
